445ff636fb44fd639948f6c1b5e99e1cf17e3953
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2019-07-24 15:00:44 -0400

    Adds starter files

---
+++ queries.sql
@@ -0,0 +1,22 @@
+-- 1. Using an INNER JOIN, retrieve the movie title (`movie_title`), year (`year`), and genre title (`genre_title`) for all the movies in the `movies` table. 
+
+
+
+-- 2. Retrieve the total number of movies in the `movies` table with the heading `Number of Movies`.
+
+
+
+-- 3. Retrieve the oldest movie year in the `movies` table in the genre 'Fantasy'.
+
+
+
+-- 4. Retrieve the number of movies for each genre title. The headings should be `Genre` and `Number of Movies`.
+
+
+
+-- 5. Retrieve a list of directors that have more than 1 movie in the `movies` table.
+
+
+
+
+
